15914f1a2e8c283080186196adf6b746020ec35f
Zengjie Zhang <zengjie.zhang@hotmail.com>
2023-10-07 14:54:50 +0200

    Update

--- main.py
+++ main.py
@@ -4,21 +4,16 @@ from commons import gen_pce_coefficients, monte_carlo_linear_bicycle, monte_carl
 from statistic import get_var_from_pce, get_mean_from_pce
 from matplotlib import pyplot
 from pce_micp_solver import PCEMICPSolver
-from param import N, phi, lanes
+from param import N, phi, lanes, v
 from commons import gen_bicycle_linear_sys
 from gen_basis import base_length, base_sampling_time
 
-x0 = np.array([0, lanes['fast'], 0, 10])
-z0 = np.array([20, lanes['slow'], 0, 50])
-
-# gamma = np.linspace(0.01, 0, N-1)
-gamma = np.linspace(0, 0, N)
-a = np.linspace(0, 0, N)
-v = np.array([gamma, a])
+x0 = np.array([0, lanes['fast'], 0, 1.2])
+z0 = np.array([1, lanes['slow'], 0, 1.2])
 
 sys = gen_bicycle_linear_sys(x0, base_sampling_time, base_length)
 
-solver = PCEMICPSolver(phi, sys, x0, z0, v, N, robustness_cost=True)
+solver = PCEMICPSolver(phi, sys, x0, z0, v, N, robustness_cost=False)
 
 Q = np.zeros([sys.n, sys.n])
 R = 0.01 * np.eye(sys.m)
--- plot_results.py
+++ plot_results.py
@@ -1,63 +1,39 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from commons import gen_pce_coefficients, monte_carlo_linear_bicycle, monte_carlo_bicycle
+from gen_basis import eta
+from param import lanes, N, v
 
 
 u = np.load('u.npy')
 x = np.load('x.npy')
 z = np.load('z.npy')
 
-plt.plot(4.8 * np.ones((30, )))
-plt.plot(3.6 * np.ones((30, )))
-plt.plot(2.4 * np.ones((30, )))
-plt.plot(1.2 * np.ones((30, )))
-plt.plot(0 * np.ones((30, )))
+H = 10
 
-plt.plot()
+plt.plot(lanes['left'] * np.ones((H, )))
+plt.plot(lanes['middle'] * np.ones((H, )))
+plt.plot(lanes['right'] * np.ones((H, )))
 
+p1, = plt.plot(x[0, :], x[1, :])
 
-plt.xlim([0, SAFETY[1]])
-plt.ylim([0, SAFETY[3]])
+# p2, = plt.plot(z[0, 0, :], z[0, 1, :])
+
+M = 64
+nodes = eta.sample([M, ])
+mc_samples = np.array([monte_carlo_bicycle(N, z[0, :, 0], v.T, node[0], node[1]) for node in nodes.T])
+
+for i in range(M):
+    plt.plot(mc_samples[i, :, 0], mc_samples[i, :, 1])
+
+plt.xlim([0, H])
+# plt.ylim([0, 5])
 plt.xlabel('x')
 plt.ylabel('y')
-plt.legend([ps, p1, p2, p3, pe],
-           ['Initial position', 'Trajectory stage 1', 'Trajectory stage 2', 'Trajectory stage 3', 'Ending position'],
-           loc='lower left')
+# plt.legend([p1, p2], ['ego', 'obstacle'], loc='lower left')
 
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
-
-fig, axs = plt.subplots(2)
-
-axs[0].plot(np.arange(0, kappa[1]+1), x_stage_1[0], color=STATE_1_COLOR, linewidth=2.5, linestyle='solid')
-axs[1].plot(np.arange(0, kappa[1]+1), x_stage_1[1], color=STATE_1_COLOR, linewidth=2.5, linestyle='solid')
-
-axs[0].plot(np.arange(kappa[1], kappa[2]+1), x_stage_2[0], color=STATE_2_COLOR, linewidth=2.5, linestyle='solid')
-axs[1].plot(np.arange(kappa[1], kappa[2]+1), x_stage_2[1], color=STATE_2_COLOR, linewidth=2.5, linestyle='solid')
-
-axs[0].plot(np.arange(kappa[2], kappa[3]+1), x_stage_3[0], color=STATE_3_COLOR, linewidth=2.5, linestyle='solid')
-axs[1].plot(np.arange(kappa[2], kappa[3]+1), x_stage_3[1], color=STATE_3_COLOR, linewidth=2.5, linestyle='solid')
-
-x_complete = np.concatenate((x_stage_1.T, x_stage_2.T[1:], x_stage_3.T[1:]))
-
-for i in range(kappa[3]):
-    if gamma_t.robustness(y=np.array([x_complete[i]]).T, t=0) >= 0:
-        axs[0].fill(*get_coordinates((i, i + 1, 0, 12)), color=TARGET_COLOR)
-        axs[1].fill(*get_coordinates((i, i + 1, 0, 12)), color=TARGET_COLOR)
-    elif gamma_h.robustness(y=np.array([x_complete[i]]).T, t=0) >= 0:
-        axs[0].fill(*get_coordinates((i, i + 1, 0, 12)), color=HOME_COLOR)
-        axs[1].fill(*get_coordinates((i, i + 1, 0, 12)), color=HOME_COLOR)
-    elif gamma_c.robustness(y=np.array([x_complete[i]]).T, t=0) >= 0:
-        axs[0].fill(*get_coordinates((i, i + 1, 0, 12)), color=CHARGER_COLOR)
-        axs[1].fill(*get_coordinates((i, i + 1, 0, 12)), color=CHARGER_COLOR)
-
-axs[0].set_xlim([0, kappa[3]])
-axs[0].set_ylim([SAFETY[2], SAFETY[3]-1])
-axs[1].set_xlim([0, kappa[3]])
-axs[1].set_ylim([SAFETY[2], SAFETY[3]-1])
-
-axs[0].set_ylabel(r'$x$')
-axs[1].set_ylabel(r'$y$')
-axs[1].set_xlabel(r'$k$')
 
 plt.show()
