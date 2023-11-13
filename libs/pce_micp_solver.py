f4181df42b62333b7b7d400cdecf0bb00b3367b4
Zengjie Zhang <zengjie.zhang@hotmail.com>
2023-11-13 17:08:41 +0100

    Update before applying new uncertainty term

--- libs/pce_micp_solver.py
+++ libs/pce_micp_solver.py
@@ -119,10 +119,10 @@ class PCEMICPSolver(STLSolver):
             self.model.addConstr(x_min <= self.x[:, t])
             self.model.addConstr(self.x[:, t] <= x_max)
 
-    def AddQuadraticCost(self, Q, R):
-        self.cost += self.x[:, 0] @ Q @ self.x[:, 0] + self.u[:, 0] @ R @ self.u[:, 0]
+    def AddQuadraticCost(self, Q, R, x_ref):
+        self.cost += (self.x[:, 0] - x_ref) @ Q @ (self.x[:, 0] - x_ref) + self.u[:, 0] @ R @ self.u[:, 0]
         for t in range(1, self.T):
-            self.cost += self.x[:, t] @ Q @ self.x[:, t] + self.u[:, t] @ R @ self.u[:, t]
+            self.cost += (self.x[:, t] - x_ref) @ Q @ (self.x[:, t] - x_ref) + self.u[:, t] @ R @ self.u[:, t]
 
         print(type(self.cost))
 
