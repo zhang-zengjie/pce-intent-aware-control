Corrected variance specification

compare_statistics.py
@@ -11,7 +11,7 @@ base_sampling_time = 0.5
 base_length = 4
 q = 2
 
-zeta_0 = np.array([10, 0.5, 0.02, 10])
+zeta_0 = np.array([10, 0.5, 0, 25])
 
 gamma = np.linspace(0, 0, N)
 a = np.linspace(0, 0, N)
@@ -23,6 +23,7 @@ np.random.seed(7)
 # tau = cp.Trunc(cp.Normal(base_sampling_time, 0.05), lower=base_sampling_time - 0.05, upper=base_sampling_time + 0.05)
 
 delta = cp.Trunc(cp.Normal(0, 0.01), lower=-0.01, upper=0.01)
+# delta = cp.Normal(0, 0.0001)
 length = cp.Uniform(lower=base_length - 1e-3, upper=base_length + 1e-3)
 eta = cp.J(delta, length)
 
@@ -58,18 +59,21 @@ for i in range(M):
 
 pce_mean = np.array([B.get_mean_from_coef(x_hat[i]) for i in range(N + 1)])
 pce_var = np.array([B.get_var_from_coef(x_hat[i]) for i in range(N + 1)])
-
+pce_std = np.array([B.get_std_from_coef(x_hat[i]) for i in range(N + 1)])
+pce_max = np.array([B.get_max_coef(x_hat[i]) for i in range(N + 1)])
+pce_coef = np.array([B.get_coef(x_hat[i], 0) for i in range(N + 1)])
 # Draw plots: mean
 '''
 pyplot.plot(np.linspace(0, 1, N+1), pce_mean.T[0])
-# pyplot.plot(np.linspace(0, 1, N+1), np.mean(mc_samples, 0).T[0])
+pyplot.plot(np.linspace(0, 1, N+1), np.mean(mc_samples, 0).T[0])
 pyplot.plot(np.linspace(0, 1, N+1), np.mean(mc_samples_linear, 0).T[0])
 
 # Draw plots: variance
-'''
-pyplot.plot(np.linspace(0, 1, N+1), pce_var.T[1])
-# pyplot.plot(np.linspace(0, 1, N+1), np.var(mc_samples, 0).T[1])
-pyplot.plot(np.linspace(0, 1, N+1), np.var(mc_samples_linear, 0).T[1])
 
+pyplot.plot(np.linspace(0, 1, N+1), pce_std.T[0])
+pyplot.plot(np.linspace(0, 1, N+1), np.std(mc_samples, 0).T[0])
+pyplot.plot(np.linspace(0, 1, N+1), np.std(mc_samples_linear, 0).T[0])
+'''
+pyplot.plot(np.linspace(0, 1, N+1), pce_max.T[0])
 
 pyplot.show()

main.py
@@ -11,7 +11,7 @@ q = 2                       # The polynomial order
 N = 30                      # The control horizon
 
 # Generate the PCE instance and the specification
-B, phi, phi_belief, phi_neg_belief = gen_pce_specs(base_sampling_time, base_length, q, N)
+B, phi, phi_belief, phi_neg_belief, mu_belief, neg_mu_belief = gen_pce_specs(base_sampling_time, base_length, q, N)
 
 # The assumed control input of the obstacle vehicle (OV)
 gamma = np.linspace(0, 0, N)
@@ -49,5 +49,6 @@ for i in range(z.shape[2]):
     xx[z.shape[1]:, i] = z[:, :, i].reshape(1, -1)[0]
 
 # Visualize the results
-
+# mu_belief.robustness(xx, 1)
+mu_belief.robustness(xx, 30)
 visualize(x, z0, v, B, sys2)