9eec2e3f998ff35e9ffd0de70167214266e03f0c
Zengjie Zhang <zengjie.zhang@hotmail.com>
2024-02-02 17:04:26 +0100

    A very very good version

--- config/uc_1_config.py
+++ config/uc_1_config.py
@@ -8,8 +8,19 @@ lanes = {'right': 1,
          'fast': 7,
          'left': 9}
 
+x_range_dict = {'switch_lane': [-5, 165],
+           'constant_speed': [25, 185],
+           'speed_up': [-5, 165],
+           'big_variance': [-5, 165]}
+
+legend_loc_dict = {'switch_lane': (0.74, 0.15),
+              'constant_speed': (0.74, 0.65),
+              'speed_up': (0.74, 0.65),
+              'big_variance': (0.74, 0.65)}
+
 eps = 0.05          # Probability threshold
-# v_lim = 30          # Velocity limit
+d_safe_x = 10
+d_safe_y = 2
 
 # Coefficients of the predicates
 o = np.zeros((4, ))
@@ -19,27 +30,27 @@ a2 = np.array([0, 1, 0, 0])
 a3 = np.array([0, 0, 1, 0])
 a4 = np.array([0, 0, 0, 1])
 
-b = 5
+def gen_pce_specs(B, N, v_lim, var_lim, sys_id):
 
-def gen_pce_specs(B, N, v_lim, sys_id):
+    mu_safe = B.probability_formula(a2, o, lanes['right'] + d_safe_y/2, eps, sys_id) | \
+        B.probability_formula(-a2, o, - lanes['left'] - d_safe_y/2, eps, sys_id) | \
+        B.probability_formula(a1, -a1, d_safe_x, eps, sys_id) | \
+        B.probability_formula(-a1, a1, d_safe_x, eps, sys_id) | \
+        B.probability_formula(a2, -a2, d_safe_y, eps, sys_id) | \
+        B.probability_formula(-a2, a2, d_safe_y, eps, sys_id)
 
-    mu_safe = B.probability_formula(a1, -a1, 10, eps, sys_id) | \
-        B.probability_formula(-a1, a1, 10, eps, sys_id) | \
-        B.probability_formula(a2, -a2, 2, eps, sys_id) | \
-        B.probability_formula(-a2, a2, 2, eps, sys_id)
-
-    mu_belief = B.variance_formula(a1, 12, sys_id) & \
+    mu_belief = B.variance_formula(a1, var_lim, sys_id) & \
         B.expectation_formula(o, -a2, -lanes['middle'], sys_id) & \
         B.expectation_formula(o, -a4, -v_lim, sys_id)
-    neg_mu_belief = B.neg_variance_formula(a1, 12, sys_id) | \
+    neg_mu_belief = B.neg_variance_formula(a1, var_lim, sys_id) | \
         B.expectation_formula(o, a2, lanes['middle'], sys_id) | \
         B.expectation_formula(o, a4, v_lim, sys_id)
 
-    mu_overtake = B.expectation_formula(a2, o, lanes['slow'] - 0.1, sys_id) & \
-        B.expectation_formula(-a2, o, - lanes['slow'] - 0.1, sys_id) & \
-        B.expectation_formula(a1, -a1, 2*b, sys_id) & \
-        B.expectation_formula(a3, o, - 1e-1, sys_id).always(0, 3) & \
-        B.expectation_formula(-a3, o, - 1e-1, sys_id).always(0, 3) 
+    mu_overtake = B.probability_formula(a2, o, lanes['slow'] - 0.1, eps, sys_id).always(0, 3) & \
+        B.probability_formula(-a2, o, - lanes['slow'] - 0.1, eps, sys_id).always(0, 3) & \
+        B.probability_formula(a1, -a1, d_safe_y, eps, sys_id).always(0, 3) & \
+        B.probability_formula(a3, o, - 1e-1, eps, sys_id).always(0, 3) & \
+        B.probability_formula(-a3, o, - 1e-1, eps, sys_id).always(0, 3) 
 
     phi_safe = mu_safe.always(0, N)
     phi_belief = mu_belief.always(0, N)
@@ -51,28 +62,34 @@ def gen_pce_specs(B, N, v_lim, sys_id):
     return phi
 
 
-def visualize(x, z, x_range, y_range, t_end):
+def visualize(x, z, T, mode):
+
+    x_range=x_range_dict[mode]
+    y_range=[0, 10]
+    legend_loc=legend_loc_dict[mode]
 
     from matplotlib.patches import Rectangle
 
-    fig, ax = plt.subplots(figsize=(5.4, 1.4))
+    fig = plt.figure(figsize=(5.4, 1.4))
+    ax = fig.add_subplot(1,1,1)
+    plt.subplots_adjust(left=0.1, right=0.99, top=0.93, bottom=0.32)
 
     plt.plot(np.arange(x_range[0], x_range[1]), lanes['left'] * np.ones((x_range[1] - x_range[0], )), linestyle='solid', linewidth=2, color='black')
     plt.plot(np.arange(x_range[0], x_range[1]), lanes['middle'] * np.ones((x_range[1] - x_range[0], )), linestyle='dashed', linewidth=1, color='black')
     plt.plot(np.arange(x_range[0], x_range[1]), lanes['right'] * np.ones((x_range[1] - x_range[0], )), linestyle='solid', linewidth=2, color='black')
 
     # Plot the trajectory of the ego vehicle (EV)
-    for j in range(0, t_end):
+    for j in range(0, T):
         pev = ax.add_patch(Rectangle(xy=(x[0, j]-4, x[1, j]-0.5), width=4, height=1, angle=x[2, j]/3.14*180, linewidth=1, 
-                               edgecolor='red', fill=True, facecolor=(255/255, 1-j/(t_end*2-2), 1-j/(t_end*2-2)), zorder=10))
+                               edgecolor='red', fill=True, facecolor=(255/255, 1-j/(T*2-2), 1-j/(T*2-2)), zorder=10))
         pov = ax.add_patch(Rectangle(xy=(z[0, j]-4, z[1, j]-0.5), width=4, height=1, angle=z[2, j]/3.14*180, linewidth=1, 
-                               edgecolor='blue', fill=True, facecolor=(1-j/(t_end*2-2), 1-j/(t_end*4-4), 255/255), zorder=10))
+                               edgecolor='blue', fill=True, facecolor=(1-j/(T*2-2), 1-j/(T*4-4), 255/255), zorder=10))
 
     plt.xlim(x_range)
     plt.ylim(y_range)
-    plt.xlabel('(m)')
-    plt.ylabel('(m)')
-    plt.legend([pev, pov], ['EV', 'OV'], loc=(0.7, 0.15), fontsize="8", ncol=2)
+    plt.xlabel('x position (m)')
+    plt.ylabel('y position (m)')
+    plt.legend([pev, pov], ['EV', 'OV'], loc=legend_loc, fontsize="8", ncol=2)
 
     plt.rcParams['pdf.fonttype'] = 42
     plt.rcParams['ps.fonttype'] = 42
--- uc_1_main.py
+++ uc_1_main.py
@@ -9,7 +9,7 @@ import math
 
 
 # The assumed control input of the obstacle vehicle (OV)
-ASSUMED_INPUT = "switch_lane"
+ASSUMED_INPUT = 'speed_up'
 Ts = 1    # The discrete sampling time Delta_t
 l = 4       # The baseline value of the vehicle length
 q = 2       # The polynomial order
@@ -19,22 +19,22 @@ v0 = 10
 np.random.seed(7)
 
 # The assumed control mode of the obstacle vehicle (OV)
-if ASSUMED_INPUT == "switch_lane":  # That the OV is trying to switch to the fast lane
+if ASSUMED_INPUT == 'switch_lane':  # That the OV is trying to switch to the fast lane
     sigma = 0.1
-    gamma = np.array([0.0005 * v0 * math.sin(i*6.28/(N-1)) for i in range(N)])
+    gamma = np.array([0.005 * math.sin(i*6.28/(N-1)) for i in range(N)])
     a = np.zeros([N, ])
     v = np.array([gamma, a])
-elif ASSUMED_INPUT == "slow_down":  # That the OV is trying to slow down (intention aware)
+elif ASSUMED_INPUT == 'constant_speed':  # That the OV is trying to slow down (intention aware)
     sigma = 0.1
     gamma = np.linspace(0, 0, N)
-    a = np.linspace(0, -2, N)
+    a = np.linspace(0, 0, N)
     v = np.array([gamma, a])
-elif ASSUMED_INPUT == "speed_up":   # That the OV is trying to speed_up (adversarial action)
+elif ASSUMED_INPUT == 'speed_up':   # That the OV is trying to speed_up (adversarial action)
     sigma = 0.1
     gamma = np.linspace(0, 0, N)
     a = np.linspace(0, 2, N)
     v = np.array([gamma, a])
-else: # "big_variance"              # That the OV maintains the current speed with big variance (naive guess)
+else: # 'big_variance'              # That the OV maintains the current speed with big variance (naive guess)
     sigma = 0.5
     gamma = np.linspace(0, 0, N)
     a = np.linspace(0, 0, N)
@@ -46,13 +46,13 @@ intent = cp.Normal(1, 1e-3)
 eta = cp.J(bias, length, intent) # Generate the random variable instance
 B = PCEBasis(eta, q)
 
-e0 = np.array([0, lanes['fast'], 0, v0*1.333])            # Initial position of the ego vehicle (EV)
+e0 = np.array([0, lanes['fast'], 0, v0*1.33])            # Initial position of the ego vehicle (EV)
 o0 = np.array([2*v0, lanes['slow'], 0, v0])           # Initial position of the obstacle vehicle (OV)
 
 # Generate the PCE instance and the specification
 
-ego = BicycleModel(e0, [0, l, 1], Ts, name="ego")                  # Dynamic model of the ego vehicle (EV)
-oppo = BicycleModel(o0, [0, l, 1], Ts, useq=v, basis=B, pce=True, name="oppo")     # Dynamic model of the obstacle vehicle (OV)
+ego = BicycleModel(e0, [0, l, 1], Ts, name='ego')                  # Dynamic model of the ego vehicle (EV)
+oppo = BicycleModel(o0, [0, l, 1], Ts, useq=v, basis=B, pce=True, name='oppo')     # Dynamic model of the obstacle vehicle (OV)
 
 # Initialize the solver
 sys = {ego.name: ego,
@@ -62,45 +62,36 @@ nodes = oppo.basis.eta.sample([N, ])
 
 UPDATE_END = N-3
 
-if False:
+if True:
 
     xx = np.zeros([ego.n, N + 1])
     zz = np.zeros([oppo.n, N + 1])
     xx[:, 0] = ego.x0
     zz[:, 0] = oppo.x0
+    u_opt = np.zeros((2, ))
 
-    for i in range(0, UPDATE_END):
+    for i in range(0, N):
         
         ego.x0 = xx[:, i]
         oppo.x0 = zz[:, i]
         oppo.param = np.array([nodes[0, i], nodes[1, i], 1])
         oppo.update_matrices()
 
-        
-        phi = gen_pce_specs(B, N-i, v0*1.2, "oppo")
-        solver = PCEMICPSolver(phi, sys, N-i, robustness_cost=True)
-        x, u, _, _ = solver.Solve()
+        if i < UPDATE_END:
+            phi = gen_pce_specs(B, N-i, v0*1.2, 20, 'oppo')
+            solver = PCEMICPSolver(phi, sys, N-i, robustness_cost=True)
+            x, u, rho, _ = solver.Solve()
+            if rho >= 0:
+                u_opt = u[:, 0]
 
-        xx[:, i + 1] = ego.f(xx[:, i], u[:, 0])
+        xx[:, i + 1] = ego.f(xx[:, i], u_opt)
         zz[:, i + 1] = oppo.f(zz[:, i], v[:, i])
 
-    for i in range(UPDATE_END, N):
-
-        ego.x0 = xx[:, i]
-        oppo.x0 = zz[:, i]
-        oppo.param = np.array([nodes[0, i], nodes[1, i], 1])
-        oppo.update_matrices()
-
-        xx[:, i + 1] = ego.f(xx[:, i], u[:, 0])
-        zz[:, i + 1] = oppo.f(zz[:, i], v[:, i])
-
-    # print(model_checking(x, z, phs, 0))
     np.save('x_' + ASSUMED_INPUT + '_c.npy', xx)
     np.save('z_' + ASSUMED_INPUT + '_c.npy', zz)
 
 else:
     xx = np.load('x_' + ASSUMED_INPUT + '_c.npy')
     zz = np.load('z_' + ASSUMED_INPUT + '_c.npy')
-    
-oppo.x0 = o0
-visualize(xx, zz, x_range=[-5, 165], y_range=[0, 10], t_end=N)
+
+visualize(xx, zz, N-3, mode=ASSUMED_INPUT)
