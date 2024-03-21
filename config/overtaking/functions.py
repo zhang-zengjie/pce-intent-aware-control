54f80070caea2c172d5b45757507730f693b108d
Zengjie Zhang <zengjie.zhang@hotmail.com>
2024-03-21 22:13:07 +0100

    Adding video scripts

--- config/overtaking/functions.py
+++ config/overtaking/functions.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import math
 from libs.pce_basis import PCEBasis
 import chaospy as cp
+from matplotlib.patches import Rectangle
+from matplotlib.animation import FFMpegWriter
+from scipy.interpolate import CubicSpline
 
 
 lanes = {'right': 1,
@@ -109,8 +112,6 @@ def visualize(x, z, mode):
     y_range=[0, 10]
     legend_loc=legend_loc_dict[mode_list[mode]]
 
-    from matplotlib.patches import Rectangle
-
     fig = plt.figure(figsize=(5.4, 1.4))
     ax = fig.add_subplot(1,1,1)
     plt.subplots_adjust(left=0.1, right=0.99, top=0.93, bottom=0.32)
@@ -150,3 +151,75 @@ def visualize(x, z, mode):
     plt.rcParams['ps.fonttype'] = 42
 
     plt.show()
+
+
+def record(x, z, mode, Ts=1, fps=12):
+
+    T = x.shape[1]
+    M = z.shape[2]
+
+    ts = np.arange(0, T)
+    tau = np.arange(0, T, 1/(fps*Ts))
+
+    x_func_x = CubicSpline(ts, x[0])
+    x_func_y = CubicSpline(ts, x[1])
+    x_func_t = CubicSpline(ts, x[2])
+    z_funcs_x = [CubicSpline(ts, z[0, :, j]) for j in range(0, M)]
+    z_funcs_y = [CubicSpline(ts, z[1, :, j]) for j in range(0, M)]
+    z_funcs_t = [CubicSpline(ts, z[2, :, j]) for j in range(0, M)]
+
+    x_range=x_range_dict[mode_list[mode]]
+    y_range=[0, 10]
+
+    metadata = dict(title='Movie', artist='Zengjie Zhang')
+    writer = FFMpegWriter(fps=fps, metadata=metadata)
+    fig = plt.figure(figsize=(8, 4.5))
+
+    plt.rcParams['animation.ffmpeg_path'] = 'C:\\Program Files (x86)\\ffmpeg-full_build\\bin\\ffmpeg.exe'
+
+    ax = fig.add_subplot(1,1,1)
+    plt.subplots_adjust(left=0.1, right=0.99, top=0.93, bottom=0.32)
+
+    plt.plot(np.arange(x_range[0], x_range[1]), lanes['left'] * np.ones((x_range[1] - x_range[0], )), linestyle='solid', linewidth=2, color='black')
+    plt.plot(np.arange(x_range[0], x_range[1]), lanes['middle'] * np.ones((x_range[1] - x_range[0], )), linestyle='dashed', linewidth=1, color='black')
+    plt.plot(np.arange(x_range[0], x_range[1]), lanes['right'] * np.ones((x_range[1] - x_range[0], )), linestyle='solid', linewidth=2, color='black')
+
+     #
+    plt.ylim([0, 10])
+    plt.xlabel('x position (m)')
+    plt.ylabel('y position (m)')
+
+    c_ego = plt.get_cmap('Reds')
+    c_oppo = plt.get_cmap('Blues')
+
+    with writer.saving(fig, 'highway_mode_' + str(mode) + '.mp4', 300):
+
+        legend_loc=legend_loc_dict[mode_list[mode]]
+
+        # Plot the trajectory of the ego vehicle (EV)
+        for i, t in enumerate(tau):
+            pov = [ax.add_patch(Rectangle(xy=(z_funcs_x[j](t)-4, z_funcs_y[j](t)-0.5), width=3.6, height=1.8, angle=z_funcs_t[j](t)/3.14*180, linewidth=1.5, linestyle='dotted',
+                                    edgecolor=(0, 0, 0.2), fill=True, facecolor=c_oppo(i*0.8/len(tau)), zorder=10+i*M+z_funcs_y[j](t)))
+                    for j in range(M)]
+
+            pev = ax.add_patch(Rectangle(xy=(x_func_x(t)-4, x_func_y(t)-0.5), width=3.6, height=1.8, angle=x_func_t(t)/3.14*180, linewidth=1.5, linestyle='dotted',
+                                    edgecolor=(0.2, 0, 0), fill=True, facecolor=c_ego(i*0.8/len(tau)), zorder=10+M*len(tau)+i*M+x_func_y(t)))
+            
+            if mode == 0:
+                plt.xlim([0.8*i - 5,  0.8*i + 25])
+            elif mode == 1:
+                plt.xlim([1.1*i - 5,  1.1*i + 25])
+            else:
+                plt.xlim([0.8*i - 5,  0.8*i + 25])
+            writer.grab_frame()
+            print('Writing frame ' + str(i) + ' out of ' + str(len(tau)))
+            pev.remove()
+            for j in range(M):
+                pov[j].remove() 
+
+        # plt.legend([pev, pov], ['EV', 'OV'], loc=legend_loc, fontsize="8", ncol=2)
+
+        plt.rcParams['pdf.fonttype'] = 42
+        plt.rcParams['ps.fonttype'] = 42
+
+        
